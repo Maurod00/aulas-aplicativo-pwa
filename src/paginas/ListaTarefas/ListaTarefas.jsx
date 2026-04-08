da872ea1e68efa39197d873787d4d40f39fe86f6
Maurod00 <mauriciosbd99@gmail.com>
2026-04-07 22:06:14 -0300

    aula dia 07/04/2026

--- src/paginas/ListaTarefas/ListaTarefas.jsx
+++ src/paginas/ListaTarefas/ListaTarefas.jsx
@@ -1,77 +1,77 @@
-import "./ListaTarefas.css"
 import { useState } from "react";
-import Principal from "../../componentes/Principal/Principal";
+import { FaTrashAlt } from "react-icons/fa";
 import CampoCustomizado from "../../componentes/CampoCustomizado/CampoCustomizado";
-import BotaoCustomizado from "../../componentes/BotaoCustomizado/BotaoCustomizado"
+import Principal from "../../componentes/Principal/Principal";
+import "./ListaTarefas.css";
 
-import { BsTrashFill } from "react-icons/bs";
-
-function ListaTarefas({}) {
+function ListaTarefas() {
   const [novaTarefa, setNovaTarefa] = useState("");
-
   const [tarefas, setTarefas] = useState([]);
 
   const adicionarTarefa = () => {
     if (novaTarefa.trim() === "") {
-      alert("Digite uma tarefa para adicionar !");
+      alert("Digite uma tarefa para adicionar!");
       return;
     }
-    console.log(novaTarefa);
 
-    setTarefas([{ concluida: false, descricao: novaTarefa}, ...tarefas]);
+    setTarefas([{ id: Date.now(), concluida: false, descricao: novaTarefa }, ...tarefas]);
     setNovaTarefa("");
   };
 
-  const removerDaLista = (indice) => {
+  const removerTarefa = (indice) => {
     if (confirm("Tem certeza que deseja remover esta tarefa?")) {
-    tarefas.splice(indice, 1);
-    setTarefas([...tarefas]);
-    };
-  }
+      tarefas.splice(indice, 1);
+      setTarefas([...tarefas]);
+    }
+  };
 
   const marcarComoConcluida = (indice) => {
-    console.log("Marcar como concluída", indice);
-    console.log("Tarefas antes", tarefas)
     tarefas[indice].concluida = !tarefas[indice].concluida;
     setTarefas([...tarefas]);
   };
 
   return (
-    <Principal>
-      <h2>Lista de Tarefas</h2>
-       
+    <Principal voltarPara="/" titulo={`Lista de Tarefas (${tarefas.length})`}>
       <div className="lista-tarefas__campo-adicionar">
-      <CampoCustomizado
-        label="Nova Tarefa"
-        id="nova-tarefa"
-        type="text"
-        placeholder="Precione Enter para adicionar"
-        value={novaTarefa}
-        onChange={(event) => setNovaTarefa(event.target.value)}
-        onKeyPress={(event) =>{
-          if(event.code === "Enter"){
-            adicionarTarefa();
-          }
-        }}
-      />
+        <CampoCustomizado
+          label="Nova Tarefa"
+          // proprieade não mapeadas...
+          id="nova-tarefa"
+          type="text"
+          placeholder="Precione Enter para adicionar"
+          value={novaTarefa}
+          onChange={(event) => setNovaTarefa(event.target.value)}
+          onKeyPress={(event) => {
+            if (event.code === "Enter") {
+              adicionarTarefa();
+            }
+          }}
+        />
+        {/* 
+        <BotaoCustomizado tipo="secundario" aoClicar={adicionarTarefa}>
+          +
+        </BotaoCustomizado> */}
+      </div>
+      {!tarefas.length && (
+        <span className="lista-tarefas__mensagem-sem-tarefas">Não há tarefas para mostrar.</span>
+      )}
 
-      <BotaoCustomizado tipo="secundario" aoClicar={adicionarTarefa}>+</BotaoCustomizado>
-      </div> 
-
-      {!tarefas.length && <span className="lista-tarefas__mensagem-sem-tarefas">Não há tarefas para mostrar.</span>}
-
-      {tarefas.map((item, indice)=> {
+      {tarefas.map((item, indice) => {
         return (
-         <div key={indice} className="lista-tarefas__tarefa">
-          <input type="checkbox" checked={item.concluida} onChange={() => marcarComoConcluida(indice)}/>
-          <span>{item.descricao}</span>
-          <BsTrashFill onClick={() => removerDaLista(indice)} />
-         </div>
-        )
-
+          <div key={indice} className="lista-tarefas__tarefa">
+            <input
+              type="checkbox"
+              checked={item.concluida}
+              onChange={() => marcarComoConcluida(indice)}
+            />
+            <span style={{ textDecoration: item.concluida ? "line-through" : "none" }}>
+              {item.descricao}
+            </span>
+            <FaTrashAlt onClick={() => removerTarefa(indice)} />
+          </div>
+        );
       })}
-
-      
+      {/* <pre>{JSON.stringify(tarefas, null, 2)}</pre> */}
     </Principal>
   );
 }
